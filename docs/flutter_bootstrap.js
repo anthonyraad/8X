c5d17bbc7dd907a089131ceb49ea02a3deea0187
anthonyraad <54115477+anthonyraad@users.noreply.github.com>
2025-12-09 22:58:45 -0800

    Add Firebase web configuration

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2085397274"
+    serviceWorkerVersion: "2672793668"
   }
 });
